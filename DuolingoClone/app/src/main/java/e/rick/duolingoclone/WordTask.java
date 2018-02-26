2a3b15a839ec92dfe2817da06a69cc19df9aa402
Rick <ricksofteng@gmail.com>
2018-02-26 11:56:57 -0700

    Initial Commit

---
+++ DuolingoClone/app/src/main/java/e/rick/duolingoclone/WordTask.java
@@ -0,0 +1,13 @@
+package e.rick.duolingoclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_task);
+    }
+}
