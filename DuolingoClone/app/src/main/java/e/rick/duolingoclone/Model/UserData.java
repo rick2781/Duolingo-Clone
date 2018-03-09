550cf1c2828b9513fadc65b026c3f4c12b277ee7
Rick <ricksofteng@gmail.com>
2018-03-09 11:05:41 -0700

    SetMethods Database

---
+++ DuolingoClone/app/src/main/java/e/rick/duolingoclone/Model/UserData.java
@@ -0,0 +1,51 @@
+package e.rick.duolingoclone.Model;
+
+/**
+ * Created by Rick on 3/9/2018.
+ */
+
+public class UserData {
+
+    String name;
+    String username;
+    String email;
+
+    public UserData(String name, String username, String email) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
